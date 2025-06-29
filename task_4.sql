4faa912d548709f22e00511307dec0f7c3e72c90
Akwasi-Twum <twumk444@gmail.com>
2025-06-29 14:10:48 +0000

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -11,3 +11,20 @@ FROM
 WHERE 
     TABLE_NAME = 'Books'
     AND TABLE_SCHEMA = DATABASE();
+-- File: task_4.sql
+-- Select the alx_book_store database
+USE alx_book_store;
+
+-- Query to get the full description of the BOOKS table
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_NAME = 'BOOKS' 
+    AND TABLE_SCHEMA = 'alx_book_store';
