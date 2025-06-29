5d2a63b27e99a523fbd4f8f4d65b9c703e2892ca
Akwasi-Twum <twumk444@gmail.com>
2025-06-29 14:12:18 +0000

    Update task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- File: task_5.sql
+-- Select the alx_book_store database
+USE alx_book_store;
+
+-- Insert a single row into the CUSTOMERS table
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
