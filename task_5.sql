ff2b503da1b45a744b34f58323f457c1bf562447
Akwasi-Twum <twumk444@gmail.com>
2025-06-29 14:22:02 +0000

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -16,3 +16,5 @@ VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 INSERT INTO CUSTOMER (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 ```
+INSERT INTO CUSTOMER (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
