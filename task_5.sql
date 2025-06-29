3ab269c5c4b7cfa58b3fcdae51a0675ad4bd5de9
Akwasi-Twum <twumk444@gmail.com>
2025-06-29 14:15:35 +0000

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -8,3 +8,11 @@ VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 -- File: task_5.sql
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+```sql name=task_5.sql
+-- File: task_5.sql
+-- Insert a single row into the CUSTOMER table
+
+INSERT INTO CUSTOMER (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+```
