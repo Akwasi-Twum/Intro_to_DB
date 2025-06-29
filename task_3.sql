b80dde42e19a1f29f323ece98005e04b1e732013
Akwasi-Twum <twumk444@gmail.com>
2025-06-29 14:28:08 +0000

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,3 +1,12 @@
 -- File: task_3.sql
 -- List all tables in the alx_book_store database
 SHOW TABLES;mysql -u your_username -p your_database_name < task_3.sql
+
+
+
+
+
+-- task_3.sql
+-- List all tables in the alx_book_store database
+
+SHOW TABLES;
